dca8de413a40770aca16dc6634dfaa086d535991
Mykhailo Afanasiuk <mykhailo.afanasiuk@cambiumnetworks.com>
2020-03-04 14:33:47 +0200

    Updated app for heroku hosting

--- src/main.ts
+++ src/main.ts
@@ -3,16 +3,17 @@ import { AppModule } from './app.module';
 import { NestExpressApplication } from '@nestjs/platform-express';
 import { join } from 'path';
 
+const port = process.env.PORT || 3000;
+
 async function bootstrap() {
-  const app = await NestFactory.create<NestExpressApplication>(AppModule, {
-    logger: ['error', 'warn', 'debug']
-  });
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+  app.enableCors();
   app.useStaticAssets(
     join(__dirname, '..', 'assets'),
     {
     prefix: '/assets/',
   });
 
-  await app.listen(3000);
+  await app.listen(port, '0.0.0.0');
 }
 bootstrap();
