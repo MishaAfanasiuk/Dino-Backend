78b7b4e2a63781f213448befbb0dff743cae697b
Mykhailo Afanasiuk <mykhailo.afanasiuk@cambiumnetworks.com>
2020-03-04 14:45:03 +0200

    Updated app for heroku hosting

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,7 +1,11 @@
 import { Controller, Get } from '@nestjs/common';
-import { AppService } from './app.service';
+
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+
+  @Get('')
+  root(){
+    return 'This is dino application backend server'
+  }
 }
